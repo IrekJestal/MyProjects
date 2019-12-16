a312025cd40167fdad6dfba67a56bc84bcea6732
IrekJestal <58946446+IrekJestal@users.noreply.github.com>
2019-12-16 16:42:49 +0100

    Update ProgramTasks.cs

--- .github/workflows/ProgramTasks.cs
+++ .github/workflows/ProgramTasks.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 namespace Test
 {
    class static void Main(string[] arg)
    {
+      
+      List<Task> li = new List<Task>();
+      Action<object> fu = (object o) => {Console.WriteLine(o);};
+
+          Task t = new Task(fu,"1");
       
    }
 }
