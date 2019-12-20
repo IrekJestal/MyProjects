507b53d90d4e5c88c201da27950f6bd64f6e3a11
IrekJestal <58946446+IrekJestal@users.noreply.github.com>
2019-12-20 17:34:01 +0100

    Create NOWAClassa.cs

---
+++ .github/workflows/NOWAClassa.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace WORD2
+{
+
+   class NOWAClassa
+  {
+     public string GetNewMesage()
+     {
+       
+        return "wywolalo funkcje";
+     }
+     
+  }
+
+  class Noew2
+  {
+      Func<int,int> f = a => a*5;
+    public string zwroc(int a)
+    { 
+        return f(a).ToString();
+    }
+
+ Func<int,int> gf = (g => g*32);
+
+int ggg(int a)
+{
+
+    int y = f(5);
+    return y;
+}
+
+private int zmien = 7;
+   private long df;
+   public int Zmien
+   {
+      get =>zmien/2;
+      set => zmien = value*6;
+   }
+
+public long reu
+{
+   get;set;
+}
+
+
+
+   string[] ordss = {"bite","bute","bawol"};
+
+  }
+}
